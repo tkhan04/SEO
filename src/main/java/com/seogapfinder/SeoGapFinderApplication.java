2b777d1e88a20fa90b9b587b6884fb6414204a18
tkhan04 <tkhanpsn@gmail.com>
2025-08-14 15:23:50 -0400

    SEO Files

---
+++ src/main/java/com/seogapfinder/SeoGapFinderApplication.java
@@ -0,0 +1,15 @@
+package com.seogapfinder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SeoGapFinderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SeoGapFinderApplication.class, args);
+	}
+
+
+	
+}
