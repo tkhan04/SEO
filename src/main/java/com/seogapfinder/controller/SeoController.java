2ca4fd307c851079512b0324d3d955146e0c87ca
tkhan04 <tkhanpsn@gmail.com>
2025-08-15 15:03:47 -0400

    just some controller adds, about to add a CORS check

---
+++ src/main/java/com/seogapfinder/controller/SeoController.java
@@ -0,0 +1,19 @@
+package com.seogapfinder.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/")
+public class SeoController {
+    
+    @GetMapping("/compare")
+    public String getQuery(@RequestParam String linkOne, @RequestParam String linkTwo) {
+
+        return "My domain: " + linkOne + "Competitor: " + linkTwo;
+    }
+
+
+}
